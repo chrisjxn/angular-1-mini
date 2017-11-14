77ab4b35ad7f4b9e8230ba59e7c8c265e1af7654
chrisjxn <usjacksons@gmail.com>
2017-11-14 12:32:15 -0700

    commit changes

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,3 +1,13 @@
 angular.module('friendsList').controller('mainCtrl', function($scope){
     $scope.friends = ['Misti', 'Eli', 'Joy', 'Faith', 'Bryant'];
+
+    $scope.add = function(friendName){
+        $scope.friends.push(friendName);
+        $scope.newFriend = "";
+    }
+
+    $scope.delete = function(name){
+        let index = $scope.friends.indexOf(name);
+        $scope.friends.splice(index, 1);
+    }
 });
